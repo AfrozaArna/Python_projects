46c0550ed01558409b8452e8945509fe82b9b3da
AfrozaArna <afroza.int.1235@gmail.com>
2023-03-10 03:28:24 +0600

    Add madlib project

---
+++ madlib.py
@@ -0,0 +1,10 @@
+verb1 = input("Verb: ")
+adj1 = input("Adjective: ")
+adverb = input("Adverb: ")
+verb2 = input("Verb: ")
+verb3 = input("Verb: ")
+adj2 = input("Adjective: ")
+
+madlib = f" Your work is going to {verb1} a {adj1} part of your life, and  the only way to be  {adverb} {verb2} is to do what you {verb3} is {adj2} work."
+
+print(madlib)
